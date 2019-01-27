5451c841ad43f8f84ce99b9b9e41f577a095c47b
Rockwelle <rwest16@holynames-sea.org>
2019-01-27 13:09:08 -0800

    created a command and subsystem for the lift, got phoenix dependencies for TalonSRX

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.I2Csubsystem;
+import frc.robot.subsystems.Lift;
 
 
 
@@ -29,6 +30,7 @@ import frc.robot.subsystems.I2Csubsystem;
 public class Robot extends TimedRobot {
   public static Drive drive = new Drive();
   public static I2Csubsystem i2Csub = new I2Csubsystem();
+  public static Lift lift = new Lift();
   public static OI m_oi;
   public static UsbCamera camera1;
   public static UsbCamera camera2;
--- src/main/java/frc/robot/commands/LiftCommand.java
+++ src/main/java/frc/robot/commands/LiftCommand.java
@@ -9,16 +9,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.RobotMap;
 
-public class LightSensor extends Command {
-
-  int readValue = -1;
-  public LightSensor() {
-    requires(Robot.i2Csub);
-    //requires(Robot.drive);
+public class LiftCommand extends Command {
+  public LiftCommand() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.lift);
   }
 
   // Called just before this Command runs the first time
@@ -29,27 +25,6 @@ public class LightSensor extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.i2Csub.readFromLight();
-    if(Robot.i2Csub.backIsReading()==RobotMap.NONE_IS_READING)
-    {
-      readValue = Robot.i2Csub.frontOnlyReading();
-    }
-    else if(Robot.i2Csub.backIsReading()!=RobotMap.NONE_IS_READING && Robot.i2Csub.frontOnlyReading() != RobotMap.NONE_IS_READING)
-    {
-      readValue = Robot.i2Csub.backIsReading();
-
-      if (readValue == RobotMap.BACK_MIDDLE_READING)
-      {
-        readValue = Robot.i2Csub.frontAndBackReading();
-
-      }
-    }
-    if(readValue == RobotMap.RIGHT_IS_READING)
-      Robot.drive.setSpeed(.15,.1);
-    if(readValue == RobotMap.LEFT_IS_READING)
-      Robot.drive.setSpeed(.1,.15);
-    if(readValue == RobotMap.FORWARD_IS_READING)
-      Robot.drive.setSpeed(.1,.1);
   }
 
   // Make this return true when this Command no longer needs to run execute()
